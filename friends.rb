2306bc6baa55e1a9b86efabf4e242fb565c1aed4
James Swatton <jamessswatton@gmail.com>
2019-04-07 17:13:49 +0100

    add likes_to_eat? func

--- friends.rb
+++ friends.rb
@@ -6,3 +6,8 @@ end
 def favourite_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+
+def likes_to_eat?(person, snack)
+  return person[:favourites][:snacks].include?(snack)
+end
--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -81,15 +81,25 @@ class TestFriends < MiniTest::Test
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person2, "bread") should return true, likes_to_eat(@person3, "spinach") should return false)
+  def test_getting_snacks
+    result = likes_to_eat?(@person2, "bread")
+    assert_equal(true, result)
+  end
 
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
-
+  # def test_how_many_friends_after_adding_new_friend
+  #   add_or_remove_friend(@person4, "Scrappy-Doo")
+  #   assert_equal(4, @person4[:friends].count)
+  # end
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-
+  # def test_how_many_friends_after_removing_friend
+  #   add_or_remove_friend(@person1, "Scooby")
+  #   assert_equal(3, @person1[:friends].count)
+  # end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
